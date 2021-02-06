319ce686aafa7d08c1f938269c2d4143cd2a3dfe
Abhishek Nage <anage@deakin.edu.au>
2021-02-05 19:55:06 -0700

    Initial commit

---
+++ phpmyadmin/index.php
@@ -0,0 +1,257 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="referrer" content="no-referrer">
+  <meta name="robots" content="noindex,nofollow">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <style id="cfs-style">html{display:none}</style>
+  <link rel="icon" href="/phpmyadmin/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="/phpmyadmin/favicon.ico" type="image/x-icon">
+      <link rel="stylesheet" type="text/css" href="/phpmyadmin/A.themes,,_pmahomme,,_jquery,,_jquery-ui.css+js,,_vendor,,_codemirror,,_lib,,_codemirror.css,,qv==5.0.4+js,,_vendor,,_codemirror,,_addon,,_hint,,_show-hint.css,,qv==5.0.4+js,,_vendor,,_codemirror,,_addon,,_lint,,_lint.css,,qv==5.0.4+themes,,_pmahomme,,_css,,_theme.css,,qv==5.0.4,,anocache==4817332203ltr,,aserver==1,Mcc.t8nMqLYASk.css.pagespeed.cf.IfKwyOWkPx.css"/>
+    
+    
+    
+    
+    <link rel="stylesheet" type="text/css" href="/phpmyadmin/themes/pmahomme/css/A.printview.css,qv=5.0.4.pagespeed.cf.pDwW6Yh62r.css" media="print" id="printcss">
+    <title>phpMyAdmin</title>
+    <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/jquery/jquery.min.js,qv=5.0.4.pagespeed.jm.tJmcu2pzqb.js"></script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/jquery/jquery-migrate.js,qv=5.0.4.pagespeed.jm.WODVtoUkr3.js"></script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/whitelist.php?v=5.0.4&amp;lang=en"></script>
+  <script src="/phpmyadmin/js/vendor,_sprintf.js,qv==5.0.4+ajax.js,qv==5.0.4+keyhandler.js,qv==5.0.4.pagespeed.jc.pm80i99PRm.js"></script><script>eval(mod_pagespeed_dhO48EfrEU);</script>
+  <script>eval(mod_pagespeed_zXC3Wvt2Tc);</script>
+  <script>eval(mod_pagespeed_a$gDyLzaT8);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/bootstrap/bootstrap.bundle.min.js,qv=5.0.4.pagespeed.jm.OBLxur523U.js"></script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/jquery/jquery-ui.min.js,qv=5.0.4.pagespeed.jm.c2YA5s-R_i.js"></script>
+  <script src="/phpmyadmin/js/vendor/js.cookie.js,qv==5.0.4+jquery,_jquery.mousewheel.js,qv==5.0.4+jquery,_jquery.event.drag-2.2.js,qv==5.0.4+jquery,_jquery.validate.js,qv==5.0.4.pagespeed.jc.1OpuzQ3Y7x.js"></script><script>eval(mod_pagespeed_mxmnZD0JeH);</script>
+  <script>eval(mod_pagespeed_5QCyVwYY6f);</script>
+  <script>eval(mod_pagespeed_yJZYKih0Z4);</script>
+  <script>eval(mod_pagespeed_QXpnRSJ7zC);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/jquery/jquery-ui-timepicker-addon.js,qv=5.0.4.pagespeed.jm.7u_CeW9B-t.js"></script>
+  <script src="/phpmyadmin/js/vendor,_jquery,_jquery.ba-hashchange-1.3.js,qv==5.0.4+vendor,_jquery,_jquery.debounce-1.0.5.js,qv==5.0.4+menu_resizer.js,qv==5.0.4+cross_framing_protection.js,qv==5.0.4+rte.js,qv==5.0.4.pagespeed.jc.IeOpwL5Nk7.js"></script><script>eval(mod_pagespeed_cijNIDzVDz);</script>
+  <script>eval(mod_pagespeed_N8DEzmyFwU);</script>
+  <script>eval(mod_pagespeed_Tv_EYvdZFq);</script>
+  <script>eval(mod_pagespeed_z9vxLPoqYp);</script>
+  <script>eval(mod_pagespeed_lZdKFv44Po);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/tracekit.js?v=5.0.4"></script>
+  <script src="/phpmyadmin/js/error_report.js,qv==5.0.4+messages.php,ql==en,av==5.0.4,alang==en+config.js,qv==5.0.4+doclinks.js,qv==5.0.4.pagespeed.jc.mqW-jtNyUW.js"></script><script>eval(mod_pagespeed_E_6yq5s9K6);</script>
+  <script>eval(mod_pagespeed_JN$$$OuhFE);</script>
+  <script>eval(mod_pagespeed_ihCRHkewO9);</script>
+  <script>eval(mod_pagespeed_Ho8HBcX8FI);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/functions.js,qv=5.0.4.pagespeed.jm.Nd8vCK_7UF.js"></script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/navigation.js,qv=5.0.4.pagespeed.jm.TAIy1AjSJw.js"></script>
+  <script src="/phpmyadmin/js/indexes.js,qv==5.0.4+common.js,qv==5.0.4+page_settings.js,qv==5.0.4+drag_drop_import.js,qv==5.0.4+shortcuts_handler.js,qv==5.0.4.pagespeed.jc.IzwPsTu93n.js"></script><script>eval(mod_pagespeed_7Vmg46f$Wq);</script>
+  <script>eval(mod_pagespeed_ee_tjKvFEY);</script>
+  <script>eval(mod_pagespeed_cFtUUDr4CI);</script>
+  <script>eval(mod_pagespeed_T2IOQU9z83);</script>
+  <script>eval(mod_pagespeed_n4QBJmq921);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/vendor/codemirror/lib/codemirror.js,qv=5.0.4.pagespeed.jm.AGCOJVkFZD.js"></script>
+  <script src="/phpmyadmin/js/vendor,_codemirror,_mode,_sql,_sql.js,qv==5.0.4+vendor,_codemirror,_addon,_runmode,_runmode.js,qv==5.0.4+vendor,_codemirror,_addon,_hint,_show-hint.js,qv==5.0.4+vendor,_codemirror,_addon,_hint,_sql-hint.js,qv==5.0.4+vendor,_codemirror,_addon,_lint,_lint.js,qv==5.0.4+codemirror,_addon,_lint,_sql-lint.js,qv==5.0.4.pagespeed.jc.2p-vqUPyp3.js"></script><script>eval(mod_pagespeed_6UEAp$zv_W);</script>
+  <script>eval(mod_pagespeed_$sw9FbL4sh);</script>
+  <script>eval(mod_pagespeed_1E98kM_Aq9);</script>
+  <script>eval(mod_pagespeed_nfWIwgfPR_);</script>
+  <script>eval(mod_pagespeed_DOss3SMLYj);</script>
+  <script>eval(mod_pagespeed_sIPozeH4Df);</script>
+  <script data-cfasync="false" type="text/javascript" src="/phpmyadmin/js/console.js,qv=5.0.4.pagespeed.jm.UxtZRbsl9G.js"></script>
+
+<script data-cfasync="false" type="text/javascript">//<![CDATA[
+CommonParams.setAll({common_query:"?lang=en",opendb_url:"db_structure.php",lang:"en",server:"1",table:"",db:"",token:"3a3f25723f2e357956505125765b362a",text_dir:"ltr",show_databases_navigation_as_tree:true,pma_text_default_tab:"Browse",pma_text_left_default_tab:"Structure",pma_text_left_default_tab2:false,LimitChars:"50",pftext:"",confirm:true,LoginCookieValidity:"1440",session_gc_maxlifetime:"1440",logged_in:false,is_https:false,rootPath:"/phpmyadmin/",arg_separator:"&",PMA_VERSION:"5.0.4",auth_type:"cookie",user:"root"});ConsoleEnterExecutes=false
+AJAX.scriptHandler.add('vendor/jquery/jquery.min.js',0).add('vendor/jquery/jquery-migrate.js',0).add('whitelist.php',1).add('vendor/sprintf.js',1).add('ajax.js',0).add('keyhandler.js',1).add('vendor/bootstrap/bootstrap.bundle.min.js',1).add('vendor/jquery/jquery-ui.min.js',0).add('vendor/js.cookie.js',1).add('vendor/jquery/jquery.mousewheel.js',0).add('vendor/jquery/jquery.event.drag-2.2.js',0).add('vendor/jquery/jquery.validate.js',0).add('vendor/jquery/jquery-ui-timepicker-addon.js',0).add('vendor/jquery/jquery.ba-hashchange-1.3.js',0).add('vendor/jquery/jquery.debounce-1.0.5.js',0).add('menu_resizer.js',1).add('cross_framing_protection.js',0).add('rte.js',1).add('vendor/tracekit.js',1).add('error_report.js',1).add('messages.php',0).add('config.js',1).add('doclinks.js',1).add('functions.js',1).add('navigation.js',1).add('indexes.js',1).add('common.js',1).add('page_settings.js',1).add('drag_drop_import.js',1).add('shortcuts_handler.js',1).add('vendor/codemirror/lib/codemirror.js',0).add('vendor/codemirror/mode/sql/sql.js',0).add('vendor/codemirror/addon/runmode/runmode.js',0).add('vendor/codemirror/addon/hint/show-hint.js',0).add('vendor/codemirror/addon/hint/sql-hint.js',0).add('vendor/codemirror/addon/lint/lint.js',0).add('codemirror/addon/lint/sql-lint.js',0).add('console.js',1);$(function(){AJAX.fireOnload('whitelist.php');AJAX.fireOnload('vendor/sprintf.js');AJAX.fireOnload('keyhandler.js');AJAX.fireOnload('vendor/bootstrap/bootstrap.bundle.min.js');AJAX.fireOnload('vendor/js.cookie.js');AJAX.fireOnload('menu_resizer.js');AJAX.fireOnload('rte.js');AJAX.fireOnload('vendor/tracekit.js');AJAX.fireOnload('error_report.js');AJAX.fireOnload('config.js');AJAX.fireOnload('doclinks.js');AJAX.fireOnload('functions.js');AJAX.fireOnload('navigation.js');AJAX.fireOnload('indexes.js');AJAX.fireOnload('common.js');AJAX.fireOnload('page_settings.js');AJAX.fireOnload('drag_drop_import.js');AJAX.fireOnload('shortcuts_handler.js');AJAX.fireOnload('console.js');});
+//]]></script>
+
+  <noscript><style>html{display:block}</style></noscript>
+</head>
+<body id=loginform>
+  
+  
+  
+
+  
+  
+  
+  
+
+  <div id="page_content">
+    
+
+  
+<div class="container">
+<a href="/phpmyadmin/url.php?url=https%3A%2F%2Fwww.phpmyadmin.net%2F" target="_blank" rel="noopener noreferrer" class="logo">
+<img src="/phpmyadmin/themes/pmahomme/img/logo_right.png.pagespeed.ce.2a4amFzu7U.png" id="imLogo" name="imLogo" alt="phpMyAdmin" border="0">
+</a>
+<h1>Welcome to <bdo dir="ltr" lang="en">phpMyAdmin</bdo></h1>
+
+<noscript>
+<div class="error"><img src="/phpmyadmin/themes/dot.gif.pagespeed.ce.MlRyYBVx8x.gif" title="" alt="" class="icon ic_s_error"> Javascript must be enabled past this point!</div>
+</noscript>
+
+<div class="hide" id="js-https-mismatch">
+<div class="error"><img src="/phpmyadmin/themes/dot.gif.pagespeed.ce.MlRyYBVx8x.gif" title="" alt="" class="icon ic_s_error"> There is a mismatch between HTTPS indicated on the server and client. This can lead to a non working phpMyAdmin or a security risk. Please fix your server configuration to indicate HTTPS properly.</div>
+</div>
+<div class='hide js-show'>    <form method="get" action="/phpmyadmin/index.php" class="disableAjax">
+    <input type="hidden" name="db" value=""><input type="hidden" name="table" value=""><input type="hidden" name="lang" value="en"><input type="hidden" name="token" value="3a3f25723f2e357956505125765b362a">
+
+            <fieldset>
+            <legend lang="en" dir="ltr">Language</legend>
+    
+    <select name="lang" class="autosubmit" lang="en" dir="ltr" id="sel-lang">
+
+                    <option value="sq">
+        Shqip - Albanian
+        </option>
+                    <option value="ar">
+        &#1575;&#1604;&#1593;&#1585;&#1576;&#1610;&#1577; - Arabic
+        </option>
+                    <option value="hy">
+        Հայերէն - Armenian
+        </option>
+                    <option value="az">
+        Az&#601;rbaycanca - Azerbaijani
+        </option>
+                    <option value="bn">
+        বাংলা - Bangla
+        </option>
+                    <option value="be">
+        &#1041;&#1077;&#1083;&#1072;&#1088;&#1091;&#1089;&#1082;&#1072;&#1103; - Belarusian
+        </option>
+                    <option value="bg">
+        &#1041;&#1098;&#1083;&#1075;&#1072;&#1088;&#1089;&#1082;&#1080; - Bulgarian
+        </option>
+                    <option value="ca">
+        Catal&agrave; - Catalan
+        </option>
+                    <option value="zh_cn">
+        &#20013;&#25991; - Chinese simplified
+        </option>
+                    <option value="zh_tw">
+        &#20013;&#25991; - Chinese traditional
+        </option>
+                    <option value="cs">
+        Čeština - Czech
+        </option>
+                    <option value="da">
+        Dansk - Danish
+        </option>
+                    <option value="nl">
+        Nederlands - Dutch
+        </option>
+                    <option value="en" selected="selected">
+        English
+        </option>
+                    <option value="en_gb">
+        English (United Kingdom)
+        </option>
+                    <option value="et">
+        Eesti - Estonian
+        </option>
+                    <option value="fi">
+        Suomi - Finnish
+        </option>
+                    <option value="fr">
+        Fran&ccedil;ais - French
+        </option>
+                    <option value="gl">
+        Galego - Galician
+        </option>
+                    <option value="de">
+        Deutsch - German
+        </option>
+                    <option value="el">
+        &Epsilon;&lambda;&lambda;&eta;&nu;&iota;&kappa;&#940; - Greek
+        </option>
+                    <option value="he">
+        &#1506;&#1489;&#1512;&#1497;&#1514; - Hebrew
+        </option>
+                    <option value="hu">
+        Magyar - Hungarian
+        </option>
+                    <option value="id">
+        Bahasa Indonesia - Indonesian
+        </option>
+                    <option value="ia">
+        Interlingua
+        </option>
+                    <option value="it">
+        Italiano - Italian
+        </option>
+                    <option value="ja">
+        &#26085;&#26412;&#35486; - Japanese
+        </option>
+                    <option value="kk">
+        Қазақ - Kazakh
+        </option>
+                    <option value="ko">
+        &#54620;&#44397;&#50612; - Korean
+        </option>
+                    <option value="lt">
+        Lietuvi&#371; - Lithuanian
+        </option>
+                    <option value="nb">
+        Norsk - Norwegian
+        </option>
+                    <option value="pl">
+        Polski - Polish
+        </option>
+                    <option value="pt">
+        Portugu&ecirc;s - Portuguese
+        </option>
+                    <option value="pt_br">
+        Portugu&ecirc;s (Brasil) - Portuguese (Brazil)
+        </option>
+                    <option value="ro">
+        Rom&acirc;n&#259; - Romanian
+        </option>
+                    <option value="ru">
+        &#1056;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081; - Russian
+        </option>
+                    <option value="sr@latin">
+        Srpski - Serbian (latin)
+        </option>
+                    <option value="si">
+        &#3523;&#3538;&#3458;&#3524;&#3517; - Sinhala
+        </option>
+                    <option value="sk">
+        Sloven&#269;ina - Slovak
+        </option>
+                    <option value="sl">
+        Sloven&scaron;&#269;ina - Slovenian
+        </option>
+                    <option value="es">
+        Espa&ntilde;ol - Spanish
+        </option>
+                    <option value="sv">
+        Svenska - Swedish
+        </option>
+                    <option value="tr">
+        T&uuml;rk&ccedil;e - Turkish
+        </option>
+                    <option value="uk">
+        &#1059;&#1082;&#1088;&#1072;&#1111;&#1085;&#1089;&#1100;&#1082;&#1072; - Ukrainian
+        </option>
+                    <option value="vi">
+        Tiếng Việt - Vietnamese
+        </option>
+    
+    </select>
+
+            </fieldset>
+    
+    </form>
+</div>
+    <br>
+    <!-- Login form -->
+    <form method="post" id="login_form" action="/phpmyadmin/index.php" name="login_form" class="disableAjax hide login js-show">
+        <fieldset>
+        <legend><input type="hidden" name="set_session" value="dvmc8c83jalr9vi8c1hsoj499b">Log in<a href="/phpmyadmin/doc/html/index.html" target="documentation"><img src="/phpmyadmin/themes/dot.gif.pagespeed.ce.MlRyYBVx8x.gif" title="Documentation" alt="Documentation" class="icon ic_b_help"></a></legend><div class="item">
+                <label for="input_username">Username:</label>
+                <input type="text" name="pma_username" id="input_username" value="" size="24" class="textfield">
+            </div>
+            <div class="item">
+                <label for="input_password">Password:</label>
+                <input type="password" name="pma_password" id="input_password" value="" size="24" class="textfield">
+            </div>    <input type="hidden" name="server" value="1"></fieldset><fieldset class="tblFooters"><input class="btn btn-primary" value="Go" type="submit" id="input_go"><input type="hidden" name="target" value="index.php"><input type="hidden" name="lang" value="en"><input type="hidden" name="token" value="3a3f25723f2e357956505125765b362a"></fieldset>
+    </form></div>
+  </div>
+  </body>
+</html>
---
+++ phpmyadmin/url.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/phpmyadmin/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/phpmyadmin/";
+		</script>
+
+		<p>You are being redirected to <a href="/phpmyadmin/">/phpmyadmin/</a></p>
+	</body>
+</html>
